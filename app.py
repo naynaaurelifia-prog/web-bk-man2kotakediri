120fa20ea8b7309527a67ccccdbcf1d12fdf0eba
naynaaurelifia-prog <naynaaurelifia@gmail.com>
2026-05-28 07:46:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,67 +5,47 @@ import os
 
 app = Flask(__name__)
 
-# Konfigurasi Database
-db_path = os.path.join('/tmp', 'database_final_v1.db') # Nama baru biar fresh
+# Konfigurasi Database - Gunakan V3 biar bener-bener fresh
+db_path = os.path.join('/tmp', 'database_v3.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Model Database
 class Pelanggaran(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nama_siswa = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=True)
-    kelas = db.Column(db.String(20), nullable=False)
-    jenis_pelanggaran = db.Column(db.String(100), nullable=True)
-    waktu = db.Column(db.DateTime, default=datetime.utcnow)
-    pelapor = db.Column(db.String(100))
+    kelas = db.Column(db.String(20), default='10')
 
+# Buat Database dan Isi Nama Teman
 with app.app_context():
     db.create_all()
-    data_kelas = {
-data_kelas = {
-        'AFRIALDY': 'afrialdy123',
-        'AHMAD FAIRUZ NADHIR AMRULLOH': 'faiz123',
-        'AHMADA DAKA ELJEISA FATIR': 'jesa123',
-        'ANNISA AZIZAH NUR AQLIS': 'annisa123',
-        'ASHFA ALYA': 'ashfa123',
-        'CINTA TARISA': 'cinta123',
-        'DAMAR SATRIO WIBOWO': 'damar123',
-        'DEVINA AURALIA IMANDA PUTRI': 'devina123',
-        'CAKRA PAMBAYUN': 'cakra123',
-        'GAYATRI NASTITI DWI HAPSARI': 'aya123',
-        'HABLY WAFIROTAL IZZAH': 'hably123',
-        'IQLIMA AINUN HANIFAH': 'hani123',
-        'JASMINE PRAMESWARI WAHYUDI': 'jasmine123',
-        'KALILA SALSABILA': 'kalila123',
-        'MALVA ANASIRUL RAHMAH': 'malva123',
-        'MOH.ANANDA RIZKY FAUZI': 'rizky123',
-        'MUHAMMAD DAVIN REZQYANO': 'davin123',
-        'MUHAMMAD GALVIN SOBIRIN': 'galvin123',
-        'MUHAMMAD HAFIDZUL BAYDHOWI': 'owi123',
-        'MUHAMMAD HAFIDZUS SALAM': 'salam123',
-        'NADHIFA AQILAH': 'nadhifa123',
-        'NAYNA KEISYA AURELIFIA': 'nayna123',
-        'NUZULIKHAN LANGIT ALFASANAH': 'zulkha123',
-        'OBBIE ABRAR RASHEESA': 'obbie123',
-        'PRINCESS ANNABELLE RAHMA HUWAIDA': 'abel123',
-        'PUTRI AYU AURA RAMADHANI': 'ayu123',
-        'RADITHYA ALTHAF HUDA RAMADHAN': 'althaf123',
-        'RAFA WAHYUZAKY ANANDIKA': 'rafa123',
-        'RAJA AULIA RIZQY PRIHARTONO': 'raja123',
-        'SEPTHIA PUTRI WAGITA': 'tia123',
-        'STEFANIE QUEEN': 'fani123',
-        'SYIFA AMELIA ARTANTI': 'syifa123',
-        'TANAYA RISWANA MAHARANI': 'naya123',
-        'VERRINSYANA VHIMALA': 'vivi123',
+    # Daftar nama temanmu
+    teman = {
+        'AFRIALDY': 'afrialdy123', 'AHMAD FAIRUZ NADHIR AMRULLOH': 'faiz123',
+        'AHMADA DAKA ELJEISA FATIR': 'jesa123', 'ANNISA AZIZAH NUR AQLIS': 'annisa123',
+        'ASHFA ALYA': 'ashfa123', 'CINTA TARISA': 'cinta123',
+        'DAMAR SATRIO WIBOWO': 'damar123', 'DEVINA AURALIA IMANDA PUTRI': 'devina123',
+        'CAKRA PAMBAYUN': 'cakra123', 'GAYATRI NASTITI DWI HAPSARI': 'aya123',
+        'HABLY WAFIROTAL IZZAH': 'hably123', 'IQLIMA AINUN HANIFAH': 'hani123',
+        'JASMINE PRAMESWARI WAHYUDI': 'jasmine123', 'KALILA SALSABILA': 'kalila123',
+        'MALVA ANASIRUL RAHMAH': 'malva123', 'MOH.ANANDA RIZKY FAUZI': 'rizky123',
+        'MUHAMMAD DAVIN REZQYANO': 'davin123', 'MUHAMMAD GALVIN SOBIRIN': 'galvin123',
+        'MUHAMMAD HAFIDZUL BAYDHOWI': 'owi123', 'MUHAMMAD HAFIDZUS SALAM': 'salam123',
+        'NADHIFA AQILAH': 'nadhifa123', 'NAYNA KEISYA AURELIFIA': 'nayna123',
+        'NUZULIKHAN LANGIT ALFASANAH': 'zulkha123', 'OBBIE ABRAR RASHEESA': 'obbie123',
+        'PRINCESS ANNABELLE RAHMA HUWAIDA': 'abel123', 'PUTRI AYU AURA RAMADHANI': 'ayu123',
+        'RADITHYA ALTHAF HUDA RAMADHAN': 'althaf123', 'RAFA WAHYUZAKY ANANDIKA': 'rafa123',
+        'RAJA AULIA RIZQY PRIHARTONO': 'raja123', 'SEPTHIA PUTRI WAGITA': 'tia123',
+        'STEFANIE QUEEN': 'fani123', 'SYIFA AMELIA ARTANTI': 'syifa123',
+        'TANAYA RISWANA MAHARANI': 'naya123', 'VERRINSYANA VHIMALA': 'vivi123',
         'ZIANKA QOLBI UDZMA ISLAMEY': 'zizi123'
     }
-    }
-    for nama, pw in data_kelas.items():
-        cek = Pelanggaran.query.filter_by(nama_siswa=nama).first()
-        if not cek:
-            db.session.add(Pelanggaran(nama_siswa=nama, password=pw, kelas='10'))
+    for nama, pw in teman.items():
+        if not Pelanggaran.query.filter_by(nama_siswa=nama).first():
+            db.session.add(Pelanggaran(nama_siswa=nama, password=pw))
     db.session.commit()
 
 @app.route('/')
@@ -80,16 +60,12 @@ def login_siswa():
         user = Pelanggaran.query.filter_by(nama_siswa=nama_input).first()
         if user and user.password == pw_input:
             return redirect(url_for('siswa'))
-        return "Login Gagal! Cek Nama (Gunakan Huruf Kapital) & Password."
+        return "Login Gagal! Cek Nama (KAPITAL) & Password."
     return render_template('login_siswa.html')
 
-@app.route('/siswa', methods=['GET', 'POST'])
+@app.route('/siswa')
 def siswa():
-    hasil = None
-    if request.method == 'POST':
-        nama_cari = request.form.get('nama')
-        hasil = Pelanggaran.query.filter(Pelanggaran.nama_siswa.like(f'%{nama_cari}%')).all()
-    return render_template('siswa.html', hasil=hasil)
+    return "Selamat datang di halaman siswa!"
 
 if __name__ == "__main__":
     app.run(debug=True)
